7ca7370dad66b51b9c08810909cac1db310e2aae
WilmarDeMelquisedecLisbet <carewaz23@hotmail.com>
2021-06-19 01:25:55 -0500

    nuevo reducer, nuevo estado, todos los paises

--- api/src/routes/activities.js
+++ api/src/routes/activities.js
@@ -4,39 +4,59 @@ const router = Router();
 const axios = require('axios').default
 
 router.post('/', async (req, res) => {
-  if(await Country.count() === 0) {
-    const respuesta = await axios.get('https://restcountries.eu/rest/v2/all');        
-        await respuesta.data.map(async pais => {
-            await Country.findOrCreate({
-                where: {
-                    id: pais.alpha3Code,
-                    nombre: pais.name,
-                    bandera: pais.flag,
-                    continente: pais.region,
-                    capital: pais.capital,
-                    subregion: pais.subregion,
-                    area: pais.area,
-                    poblacion: pais.population
-                }            
-            });
-        });
-  } else {
-    const { nombre, dificultad, duracion, temporada, paisesId } = req.body
-    try {
-      const [actividad, creada] = await Activity.findOrCreate({
 
-        where: {nombre, dificultad, duracion, temporada}
-    
-      })
-      if(creada) {
-        await actividad.addCountries(paisesId)
-        return res.json({mensaje: `La actividad se asignó a ${paisesId}`})
-      }else{
-        paisesId.map(async id => !await actividad.hasCountry(id) && await actividad.addCountry(id))
-        res.json({mensaje: `La actividad con id ${actividad.id} ha sido asignada al país que no la tenía`})
+  if(await Country.count() === 0) {
+
+    const respuesta = await axios.get('https://restcountries.eu/rest/v2/all')       
+    await respuesta.data.map(async pais => {
+        await Country.findOrCreate({
+            where: {
+                id: pais.alpha3Code,
+                nombre: pais.name,
+                bandera: pais.flag,
+                continente: pais.region,
+                capital: pais.capital,
+                subregion: pais.subregion,
+                area: pais.area,
+                poblacion: pais.population
+            }            
+        });
+    });
+
+  } else {
+
+    const { nombre, dificultad, duracion, temporada, paisesId } = req.body
+
+    if(nombre && paisesId?.length){
+
+      const paisesIdUnicos = [... new Set(paisesId)]
+
+      try {
+        const [actividad, creada] = await Activity.findOrCreate({
+
+          where: {nombre, dificultad, duracion, temporada}
+      
+        })
+        if(creada) {
+          await actividad.addCountries(paisesIdUnicos)
+
+          return res.json({mensaje: `La actividad "${actividad.nombre}" se asignó a ${paisesIdUnicos.toString()}`})
+
+        }else{
+          paisesIdUnicos.map(async id => !await actividad.hasCountry(id) && await actividad.addCountry(id))
+
+          return res.json({mensaje: `Si algún pais de la lista no contaba con la actividad  ${actividad.id} ha sido asignada correctamente`}) 
+          
+        }
+      } catch (error) {
+        return res.json({mensaje: error})
       }
-    } catch (error) {
-      return res.json({mensaje: error})
+    }else{
+      return !nombre && paisesId.length ? res.json({mensaje: 'El nombre es requerido'}) 
+      :
+      nombre && !paisesId?.length ? res.json({mensaje: 'Se requieren paises para asignar la actividad'}) 
+      :
+      res.json({mensaje: 'Es necesario el nombre de la actividad y los paises a asignar dicha actividad'})
     }
   }  
 });
