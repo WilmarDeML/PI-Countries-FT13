2a1ed624a6a161444e5a3d6a90e56e4a3f76baf2
Wilmar Zapata <wavilla@stefanini.com>
2022-03-01 16:25:08 -0500

    Cambio a la landing page

--- client/src/componentes/home/Home.js
+++ client/src/componentes/home/Home.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { obtenerTodos } from '../../actions'
@@ -14,33 +13,23 @@ const Home = () => {
     }, [dispatch])
 
     return (
-        <>
-        <section className='seccionBanderasTop'>
-            {
-                paises &&                   
-                    paises.map( (pais, indice) => (								
-                        indice <= paises.length/2 && <div key={pais.id} value={pais.id} className='imgBandera' >                                   
-                            <img src={pais.bandera} alt='ups...' style={{height: '2em', width: '3em'}} />                            
-                        </div>								        
-                    ))
-            }
-        </section>
-        <section >
-            <Link to='/countries' className='seccionEnlace'>
-                <h1>Página principal para la información de los paises en el mundo!</h1>
-            </Link>        
-        </section>
-        <section className='seccionBanderasBottom'>
-            {
-                paises &&                   
-                    paises.map( (pais, indice) => (								
-                        indice >= paises.length/2 && <div key={pais.id} value={pais.id} >                                   
-                            <img src={pais.bandera} alt='ups...' style={{height: '2em', width: '3em'}} className='imgBandera' />                            
-                        </div>								        
-                    ))
-            }
-        </section>
-        </>
+        <div className='contenedor'>
+            <section className='seccionBanderas'>
+                {
+                    paises &&                   
+                        paises.map( (pais, indice) => (								
+                            indice <= paises.length && <figure key={pais.id} value={pais.id} className='imgBandera' >                                   
+                                <img src={pais.bandera} alt='ups...' />                            
+                            </figure>								        
+                        ))
+                }
+            </section>
+            <section >
+                <Link to='/countries' className='seccionEnlace'>
+                    <h1>Página principal para la información de los paises en el mundo!</h1>
+                </Link>        
+            </section>
+        </div>
     )
 }
 
